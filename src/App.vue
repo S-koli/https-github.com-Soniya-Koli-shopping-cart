78c9b95379134b7676594d3d34a62f4c347489a1
Suraj <kunwarsuraj19@gmail.com>
2023-06-03 15:15:47 +0530

    first Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<TheHeader :page="page" :cart="cart" @togglePage="togglePage"></TheHeader>
+<ProductItem v-if="page==='product'" :cart="cart" @addToCart="addToCart"></ProductItem>
+<CartPage v-if="page==='cart'"   :cart="cart"  @removeFromCart="removeFromCart"></CartPage>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+ import TheHeader from "../src/components/UI/TheHeader.vue"
+import ProductItem from "./components/Products/ProductItem.vue"
+import CartPage from "./components/ShoppingCart/CartPage.vue"
+export default{
+ components :{
+  ProductItem,
+  CartPage,
+  TheHeader
+ } ,
+ data (){
+  return {
+    page : "product",
+    cart :[]
   }
-}
+ },
+ methods :{
+  togglePage(){
+    if(this.page === "product"){
+      this.page = "cart"
+    }else if (this.page === "cart"){
+      this.page = "product"
+    }
+  },
+  addToCart(product){
+    this.cart.push(product)
+  },
+  removeFromCart(productIdx){
+    console.log(productIdx)
+     this.cart.splice(productIdx, 1)
+ //console.log(this.cart);
+  //console.log(product)
+  }
+ }
+
+ }
+
+
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  padding: 0px;
+  margin: 0px;
+  box-sizing: border-box; 
 }
+
 </style>
